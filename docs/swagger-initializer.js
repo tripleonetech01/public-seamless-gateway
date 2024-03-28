f7222c71e62fef93dbecf88c31d431f928a5bc9a
tripleonetech01 <164311702+tripleonetech01@users.noreply.github.com>
2024-03-28 15:42:29 +0800

    add ui

---
+++ docs/swagger-initializer.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    // url: "swagger.yaml",
+    urls: [
+      {url: "/seamless-gateway-api.yml", name: "Seamless Gateway"},
+      {url: "/remote-wallet.yml", name: "Remote Wallet"}
+    ],
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
